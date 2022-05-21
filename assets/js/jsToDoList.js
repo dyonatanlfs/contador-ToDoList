ab8f4d64413d08dca78aa225f67679276b833468
dyonatanlfs <dyonatan55@hotmail.com>
2022-05-21 15:18:22 -0300

    Inserção do layout feito com html e css do desafio To Do List.

---
+++ assets/js/jsToDoList.js
@@ -0,0 +1,7 @@
+var listafazeresWrapper = document.getElementById('listafazeres');
+var listafazeres = 0;
+
+function newitemlist() {
+    listafazeres = listafazeres;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
--- assets/js/jscontador.js
+++ assets/js/jscontador.js
@@ -1,12 +1,12 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
-function increment () {
+function increment() {
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
 
-function decrement () {
+function decrement() {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
