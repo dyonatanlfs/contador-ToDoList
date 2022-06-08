631d45aba39be94bcba5a0850987e1e1bfe066ad
dyonatanlfs <dyonatan55@hotmail.com>
2022-06-08 10:20:18 -0300

    Inserção final do projeto contador proposto pela DIO e oferecido pela TQI.

--- assets/js/jsToDoList.js
+++
@@ -1,7 +0,0 @@
-var listafazeresWrapper = document.getElementById('listafazeres');
-var listafazeres = 0;
-
-function newitemlist() {
-    listafazeres = listafazeres;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
